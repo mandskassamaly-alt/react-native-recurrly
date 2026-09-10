fb96b74a6068825aaf4781b30fc9df4c61ee31cd
manda-hue <manda@intern-mail.metabox.technology>
2026-09-10 15:21:29 +0300

    first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,31 @@
+import "@/global.css"
+import { Text, View } from "react-native";
+import {Link} from "expo-router";
+import { Pressable} from "react-native";
+
+export default function App() {
+  return (
+      <View className="flex-1 items-center justify-center bg-background">
+        <Text className="text-xl font-bold text-success">
+          Welcome to Nativewind!
+        </Text>
+          <Link href="/OnBoarding" asChild>
+              <Pressable className="mt-4 rounded bg-primary p-4">
+                  <Text className="text-center text-white">Go to Onboarding</Text>
+              </Pressable>
+          </Link>
+
+          <Link href="/(auth)/sign-in" asChild>
+              <Pressable className="mt-4 rounded bg-primary p-4">
+                  <Text className="text-center text-white">Go to Sign in</Text>
+              </Pressable>
+          </Link>
+
+          <Link href="/(auth)/sign-up" asChild>
+              <Pressable className="mt-4 rounded bg-primary p-4">
+                  <Text className="text-center text-white">Go to Sign up</Text>
+              </Pressable>
+          </Link>
+      </View>
+  );
+}
---
+++ app/_layout.tsx
@@ -0,0 +1,6 @@
+import { Stack } from "expo-router";
+import './';
+
+export default function RootLayout() {
+  return <Stack />;
+}
