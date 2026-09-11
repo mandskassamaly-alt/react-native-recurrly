cd6aeec67d50122aa88dbeacdbe91e959cb88fdb
manda-hue <manda@intern-mail.metabox.technology>
2026-09-11 09:06:42 +0300

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,48 +1,36 @@
 import "@/global.css";
-import { Text, View } from "react-native";
+import { Text } from "react-native";
 import { Link } from "expo-router";
 import { Pressable } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 export default function App() {
   return (
       <SafeAreaView className="flex-1 bg-background p-5">
-        <Text className="text-xl font-bold text-success">
-          Welcome to Nativewind!
+        <Text className="text-7xl font-bold ">
+          Home
         </Text>
+          <Text className="text-7xl font-sans-extrabold ">
+              Home
+          </Text>
           <Link href="/OnBoarding" asChild>
               <Pressable className="mt-4 rounded bg-primary p-4">
-                  <Text className="text-center text-white">Go to Onboarding</Text>
+                  <Text className="text-center text-white  font-sans-bold">Go to Onboarding</Text>
               </Pressable>
           </Link>
 
           <Link href="/(auth)/sign-in" asChild>
               <Pressable className="mt-4 rounded bg-primary p-4">
-                  <Text className="text-center text-white">Go to Sign in</Text>
+                  <Text className="text-center text-white font-sans-bold">Go to Sign in</Text>
               </Pressable>
           </Link>
 
           <Link href="/(auth)/sign-up" asChild>
               <Pressable className="mt-4 rounded bg-primary p-4">
-                  <Text className="text-center text-white">Go to Sign up</Text>
+                  <Text className="text-center text-white font-sans-bold">Go to Sign up</Text>
               </Pressable>
           </Link>
 
-          <Link
-              href={{
-                  pathname: "/subscriptions/[id]",
-                  params: { id: "spotify" },
-              }}
-          >
-              Spotify Subscription
-          </Link>
-          <Link
-              href={{
-                  pathname: "/subscriptions/[id]",
-                  params: { id: "claude" },
-              }}
-          >
-              Claude Max Subscription
-          </Link>
+
 
       </SafeAreaView>
   );
--- app/(tabs)/insights.tsx
+++ app/(tabs)/insights.tsx
@@ -1,5 +1,5 @@
 import "@/global.css";
-import { View, Text } from "react-native";
+import {  Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Insights = () => {
